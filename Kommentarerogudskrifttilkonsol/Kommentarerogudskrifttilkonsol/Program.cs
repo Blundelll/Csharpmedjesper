3dd5c6229e464d35991749482847f1d04db00819
Blundell <53934702+Blundelll@users.noreply.github.com>
2021-11-03 10:23:48 +0100

    Add files via upload

---
+++ Kommentarerogudskrifttilkonsol/Kommentarerogudskrifttilkonsol/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kommentarerogudskrifttilkonsol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //start 
+            Console.WriteLine("Jesper er syg");
+            Console.Write("Press Any Key To Continue....");
+            Console.ReadKey();
+            //slut
+        }
+    }
+}
