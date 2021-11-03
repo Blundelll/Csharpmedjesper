c886b7183720b650f5d559f4fd9fedf2b4188a69
Blundell <53934702+Blundelll@users.noreply.github.com>
2021-11-03 13:08:24 +0100

    Add files via upload

---
+++ Aritmetiske/Aritmetiske/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aritmetiske
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1 + 1; //den kan matmatik :) 
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
