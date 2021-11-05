fe3420137d89830ffe2e26ac06b17492637daf8e
Blundell <53934702+Blundelll@users.noreply.github.com>
2021-11-05 08:45:32 +0100

    Add files via upload

---
+++ Syntaktisksukkerogklistreplus/Syntaktisksukkerogklistreplus/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace Syntaktisksukkerogklistreplus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            int otherValue = 1;
+            // værdien i value er = 0
+            Console.WriteLine("Værdien i value er = " + (value + otherValue));
+            Console.ReadKey();
+
+
+
+
+
+
+            Console.Clear();
+
+
+            //Øvelse 1
+            int Counter = 0;
+            Console.WriteLine(Counter + 5);
+            Console.ReadKey();
+
+
+            Console.Clear();
+
+            //Øvelse 2
+            Console.WriteLine(++Counter);
+            Console.ReadKey();
+
+            Console.Clear();
+
+            //Øvelse 3
+            //++ før er bedre end ++ efter :)
+        }
+    }
+}
